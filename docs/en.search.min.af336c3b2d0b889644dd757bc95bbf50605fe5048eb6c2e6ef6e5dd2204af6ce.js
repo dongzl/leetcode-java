1648a3dc3d68f0e2e88a006de0769741cdcb2893
Zonglei Dong <dongzonglei@jd.com>
2020-11-27 15:41:00 +0800

    update image url.

--- docs/en.search.min.af336c3b2d0b889644dd757bc95bbf50605fe5048eb6c2e6ef6e5dd2204af6ce.js
+++ docs/en.search.min.af336c3b2d0b889644dd757bc95bbf50605fe5048eb6c2e6ef6e5dd2204af6ce.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/leetcode-java/flexsearch.min.js');loadScript('/leetcode-java/en.search-data.min.8273d0a00a2d0a29e802a261ba8ab18ba99f0454f9ed44720e6fdb2fe95dd517.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/leetcode-java/flexsearch.min.js');loadScript('/leetcode-java/en.search-data.min.903ba8706f0e7a2d56f0a20caa353f1135d0ea94233bb818c22ef64281007bd6.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
